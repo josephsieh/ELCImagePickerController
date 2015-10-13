e0eddd151891c577cd749ee3f400505ef9554e90
amitbobade2007 <synerzip@mac-pro132.synerzipune.local>
2015-10-13 16:55:41 +0530

    Photos Library Support
    - iOS 8 and above support with Photos Library.
    - iCloud sync support.

--- Classes/ELCImagePickerDemoViewController.m
+++ Classes/ELCImagePickerDemoViewController.m
@@ -9,6 +9,8 @@
 #import "ELCImagePickerDemoAppDelegate.h"
 #import "ELCImagePickerDemoViewController.h"
 #import <MobileCoreServices/UTCoreTypes.h>
+#import <Photos/Photos.h>
+#import "ELCConstants.h"
 
 
 @interface ELCImagePickerDemoViewController ()
@@ -38,28 +40,41 @@
 
 - (IBAction)launchSpecialController
 {
-    ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
-    self.specialLibrary = library;
-    NSMutableArray *groups = [NSMutableArray array];
-    [_specialLibrary enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
-        if (group) {
-            [groups addObject:group];
-        } else {
-            // this is the end
-            [self displayPickerForGroup:[groups objectAtIndex:0]];
-        }
-    } failureBlock:^(NSError *error) {
-        self.chosenImages = nil;
-        UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"Error" message:[NSString stringWithFormat:@"Album Error: %@ - %@", [error localizedDescription], [error localizedRecoverySuggestion]] delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
-        [alert show];
+    //add photo framework for ios 8 and above
+    
+    if(!IS_IOS8) {
+        ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+        self.specialLibrary = library;
+        NSMutableArray *groups = [NSMutableArray array];
+        [_specialLibrary enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+            if (group) {
+                [groups addObject:group];
+            } else {
+                // this is the end
+                [self displayPickerForGroup:[groups objectAtIndex:0]];
+            }
+        } failureBlock:^(NSError *error) {
+            self.chosenImages = nil;
+            UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"Error" message:[NSString stringWithFormat:@"Album Error: %@ - %@", [error localizedDescription], [error localizedRecoverySuggestion]] delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+            [alert show];
+            
+            NSLog(@"A problem occured %@", [error description]);
+            // an error here means that the asset groups were inaccessable.
+            // Maybe the user or system preferences refused access.
+        }];
+    }else {
+        //Get All Saved Photos
+        //Fetch PHAssetCollections:
+        PHFetchOptions *options = [[PHFetchOptions alloc] init];
+        options.predicate = [NSPredicate predicateWithFormat:@"mediaType in %@", @[@(PHAssetMediaTypeImage)]];
+        options.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"creationDate" ascending:NO]];
+        PHFetchResult *assetsFetchResult = [PHAsset fetchAssetsWithOptions:options];
         
-        NSLog(@"A problem occured %@", [error description]);
-        // an error here means that the asset groups were inaccessable.
-        // Maybe the user or system preferences refused access.
-    }];
+        [self displayPickerForGroup:assetsFetchResult];
+    }
 }
 
-- (void)displayPickerForGroup:(ALAssetsGroup *)group
+- (void)displayPickerForGroup:(NSObject *)group
 {
 	ELCAssetTablePicker *tablePicker = [[ELCAssetTablePicker alloc] initWithStyle:UITableViewStylePlain];
     tablePicker.singleSelection = YES;
@@ -75,7 +90,9 @@
     
     // Move me
     tablePicker.assetGroup = group;
-    [tablePicker.assetGroup setAssetsFilter:[ALAssetsFilter allAssets]];
+    if (!IS_IOS8) {
+        [((ALAssetsGroup*)tablePicker.assetGroup) setAssetsFilter:[ALAssetsFilter allAssets]];
+    }
     
     [self presentViewController:elcPicker animated:YES completion:nil];
 }
@@ -103,41 +120,83 @@
 	workingFrame.origin.x = 0;
     
     NSMutableArray *images = [NSMutableArray arrayWithCapacity:[info count]];
-	for (NSDictionary *dict in info) {
-        if ([dict objectForKey:UIImagePickerControllerMediaType] == ALAssetTypePhoto){
-            if ([dict objectForKey:UIImagePickerControllerOriginalImage]){
-                UIImage* image=[dict objectForKey:UIImagePickerControllerOriginalImage];
-                [images addObject:image];
-                
-                UIImageView *imageview = [[UIImageView alloc] initWithImage:image];
-                [imageview setContentMode:UIViewContentModeScaleAspectFit];
-                imageview.frame = workingFrame;
-                
-                [_scrollView addSubview:imageview];
-                
-                workingFrame.origin.x = workingFrame.origin.x + workingFrame.size.width;
-            } else {
-                NSLog(@"UIImagePickerControllerReferenceURL = %@", dict);
-            }
-        } else if ([dict objectForKey:UIImagePickerControllerMediaType] == ALAssetTypeVideo){
-            if ([dict objectForKey:UIImagePickerControllerOriginalImage]){
-                UIImage* image=[dict objectForKey:UIImagePickerControllerOriginalImage];
+    if(!IS_IOS8) {
+        for (NSDictionary *dict in info) {
+            if ([dict objectForKey:UIImagePickerControllerMediaType] == ALAssetTypePhoto){
+                if ([dict objectForKey:UIImagePickerControllerOriginalImage]){
+                    UIImage* image=[dict objectForKey:UIImagePickerControllerOriginalImage];
+                    [images addObject:image];
+                    
+                    UIImageView *imageview = [[UIImageView alloc] initWithImage:image];
+                    [imageview setContentMode:UIViewContentModeScaleAspectFit];
+                    imageview.frame = workingFrame;
+                    
+                    [_scrollView addSubview:imageview];
+                    
+                    workingFrame.origin.x = workingFrame.origin.x + workingFrame.size.width;
+                } else {
+                    NSLog(@"UIImagePickerControllerReferenceURL = %@", dict);
+                }
+            } else if ([dict objectForKey:UIImagePickerControllerMediaType] == ALAssetTypeVideo){
+                if ([dict objectForKey:UIImagePickerControllerOriginalImage]){
+                    UIImage* image=[dict objectForKey:UIImagePickerControllerOriginalImage];
 
-                [images addObject:image];
+                    [images addObject:image];
+                    
+                    UIImageView *imageview = [[UIImageView alloc] initWithImage:image];
+                    [imageview setContentMode:UIViewContentModeScaleAspectFit];
+                    imageview.frame = workingFrame;
+                    
+                    [_scrollView addSubview:imageview];
+                    
+                    workingFrame.origin.x = workingFrame.origin.x + workingFrame.size.width;
+                } else {
+                    NSLog(@"UIImagePickerControllerReferenceURL = %@", dict);
+                }
+            } else {
+                NSLog(@"Uknown asset type");
+            }
+        }
+    }else {
+        for (PHAsset *asset in info) {
+            
+            
+            
+            if (asset.mediaType == PHAssetMediaTypeImage){
                 
-                UIImageView *imageview = [[UIImageView alloc] initWithImage:image];
+                UIImageView *imageview = [[UIImageView alloc] init];
                 [imageview setContentMode:UIViewContentModeScaleAspectFit];
                 imageview.frame = workingFrame;
+                
+                
+                PHImageRequestOptions *options = [[PHImageRequestOptions alloc] init];
+                
+                // Download from cloud if necessary
+                options.networkAccessAllowed = YES;
+                options.progressHandler = ^(double progress, NSError *error, BOOL *stop, NSDictionary *info) {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        //you can update progress here
+                    });
+                };
+                
+                
+                [[PHImageManager defaultManager] requestImageForAsset:asset targetSize:workingFrame.size contentMode:PHImageContentModeAspectFit options:options resultHandler:^(UIImage *result, NSDictionary *info) {
+                    if (result) {
+                        imageview.image = result;
+                        [images addObject:result];
+                    }
+                }];
+                
+                
                 
                 [_scrollView addSubview:imageview];
                 
                 workingFrame.origin.x = workingFrame.origin.x + workingFrame.size.width;
             } else {
-                NSLog(@"UIImagePickerControllerReferenceURL = %@", dict);
+                
             }
-        } else {
-            NSLog(@"Uknown asset type");
         }
+        
     }
     
     self.chosenImages = images;
